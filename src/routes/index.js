a4adb79a45402b42cf8eb949cbc930ea5ccba96a
Daniel Rodriguez Escalante <54157496+drodriguezescalante@users.noreply.github.com>
2022-12-30 17:12:31 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,8 @@ fs.readdirSync(pathRouter).filter((file) => {
 });
 
 router.get('*', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.status(404);
   res.send({ error: 'Not found' });
 });
